7260f94a06f75c62301391b7c5a5a102fa083a9c
AshwinSri23 <60453772+AshwinSri23@users.noreply.github.com>
2024-03-27 13:47:20 -0400

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,65 @@
+CREATE TABLE Members(
+	memberId SERIAL PRIMARY KEY,
+	firstName VARCHAR(16) NOT NULL,
+	lastName VARCHAR(16) NOT NULL,
+	age NOT NULL INT,
+	weight NOT NULL INT,
+	height NOT NULL INT,
+	bmi INT,
+	restingHeartRate NOT NULL INT,
+	caloriesBurned INT NOT NULL DEFAULT 0
+	numOfKm_ran INT DEFAULT 0
+	membershipType VARCHAR(10) NOT NULL DEFAULT 'None',
+	username VARCHAR(16) NOT NULL,
+	password VARCHAR(12) NOT NULL
+);
+
+CREATE TABLE Trainer(
+	trainer_id SERIAL PRIMARY KEY,
+	firstName VARCHAR(16) NOT NULL,
+	lastName VARCHAR(16) NOT NULL,
+	username VARCHAR(16) NOT NULL,
+	password VARCHAR(12) NOT NULL
+
+);
+
+CREATE TABLE Administrator(
+	adminId SERIAL PRIMARY KEY,
+	firstName VARCHAR(16) NOT NULL,
+	lastName VARCHAR(16) NOT NULL,
+	username VARCHAR(16) NOT NULL,
+	password VARCHAR(12) NOT NULL
+
+
+);
+
+CREATE TABLE Exercises(
+	routineName VARCHAR(30) NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	description TEXT,
+	reps INT NOT NULL DEFAULT 1,
+	sets INT NOT NULL DEFAULT 1,
+	FOREIGN KEY(member_id)
+		REFERENCES Members(member_id)
+);
+
+CREATE TABLE FitnessGoals(
+	goalName VARCHAR(30) PRIMARY KEY,
+	deadLine DATE NOT NULL,
+	description TEXT,
+	type VARCHAR(20) NOT NULL,
+	commitment INT NOT NULL DEFAULT 1,
+	currentPr INT NOT NULL DEFAULT 0,
+	FOREIGN KEY(member_id)
+		REFERENCES Members(member_id)
+	
+);
+
+CREATE TABLE TrainerAvailabilities(
+	Day VARCHAR(10)  
+	PRIMARY KEY(Day,startTime,endTime)
+	FOREIGN KEY(member_id)
+		REFERENCES Members(member_id)
+
+);
+
