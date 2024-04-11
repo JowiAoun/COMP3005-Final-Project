33277b247cabf2b35a025925ea93113126fed5ff
AshwinSri23 <60453772+AshwinSri23@users.noreply.github.com>
2024-04-11 16:43:24 -0400

    Update init.sql

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -70,7 +70,7 @@ CREATE TABLE FitnessGoals(
 );
 
 CREATE TABLE TrainerAvailabilities(
-	Day VARCHAR(10),
+	day VARCHAR(10),
 	startTime TIME NOT NULL,
 	endTime TIME NOT NULL,
 	trainerId INT,
@@ -110,6 +110,8 @@ CREATE TABLE Session(
 	description TEXT,
 	startDate DATE DEFAULT CURRENT_DATE,
 	endDATE DATE DEFAULT CURRENT_DATE,
+	startTIME TIME,
+	endTime TIME,
 	trainerId INT,
 	roomNumber INT,
 	adminId INT,
@@ -158,6 +160,13 @@ CREATE TABLE RoutineContains(
 	FOREIGN KEY(routineId)
 		REFERENCES Routine(routineId)
 );
+
+CREATE TABLE SessionSchedule(
+	sessionId INT,
+	day VARCHAR(32),
+	FOREIGN KEY(sessionId)
+		REFERENCES Session(sessionId)
+);
 -- DML
 INSERT INTO Members (firstName, lastName, age, weight, height, bmi, restingHeartRate, caloriesBurned, numOfKm_ran, membershipType, username, password)
 VALUES ('John', 'Doe', 30, 180, 70, 25, 70, 1500, 10, 'Gold', 'john_doe', 'password123'),
@@ -187,7 +196,7 @@ INSERT INTO FitnessGoals (goalName, deadLine, description, type, commitment, cur
 VALUES ('Weight Loss', '2024-06-30', 'Lose 10kg in 3 months', 'Weight', 3, 0, 1),
        ('Muscle Gain', '2024-08-31', 'Gain 5kg of muscle mass', 'Muscle', 3, 0, 2);
 
-INSERT INTO TrainerAvailabilities (Day, startTime, endTime, trainerId)
+INSERT INTO TrainerAvailabilities (day, startTime, endTime, trainerId)
 VALUES ('Monday', '10:00:00', '13:00:00', 1),
        ('Wednesday', '9:00:00', '12:00:00', 2);
 
@@ -204,9 +213,9 @@ VALUES (100,20,true),
 
 
 --Initializing the tables that have foreign keys--
-INSERT INTO Session (type,capacity,name,description,startDate,endDate,trainerId,roomNumber,adminId)
-VALUES ('Personal', 1, 'Training Session', 'Simple training session','2024-09-01','2024-10-01',1,202,1),
-       ('Group', 1, 'Badminton', 'Beginner friendly class for learning 	Badminton','2024-08-12','2024-09-01',2,203,2);
+INSERT INTO Session (type,capacity,name,description,startDate,endDate,startTime,endTime,trainerId,roomNumber,adminId)
+VALUES ('Personal', 1, 'Training Session', 'Simple training session','2024-09-01','2024-10-01','10:00:00','12:00:00',1,202,1),
+       ('Group', 1, 'Badminton', 'Beginner friendly class for learning 	Badminton','2024-08-12','2024-09-01','9:00:00','10:00:00',2,203,2);
 
 
 
@@ -226,3 +235,7 @@ VALUES (1,'Personal'),
 INSERT INTO RoutineContains(exerciseId, routineId) 
 VALUES (1,1),
        (2,2); 
+
+INSERT INTO SessionSchedule(sessionId,day)
+VALUES (1,'Wednesday'),
+       (2,'Monday');	
