8cb7cf7f1fcde0856df48555f0c7598a2494cb22
isharghura <ishar.ghuras@gmail.com>
2024-04-12 20:49:03 -0400

    Added getCurrentSessions

--- server/app.py
+++ server/app.py
@@ -862,3 +862,30 @@ def getSessions():
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)})
+
+
+@app.route("/getCurrentSessions/<int:memberId>", methods=["GET"])
+def getCurrentSessions(memberId):
+    try:
+        cur.execute(
+            """
+                    SELECT *
+                    FROM Session 
+                    WHERE memberId = %s
+                    """,
+            (memberId,),
+        )
+        columns = [desc[0] for desc in cur.description]
+        results = cur.fetchall()
+        data = []
+        for row in results:
+            row_data = dict(zip(columns, row))
+            for key, value in row_data.items():
+                if isinstance(value, datetime.time):
+                    row_data[key] = str(value)
+            data.append(row_data)
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)})
